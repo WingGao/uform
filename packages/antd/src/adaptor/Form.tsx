d04d7af9619d3bc3f1bc632a89ac9fbd26995151
WingGao <wing.gao@live.com>
2020-05-29 17:03:08 +0800

    feat: add spin

--- packages/antd/src/adaptor/Form.tsx
+++ packages/antd/src/adaptor/Form.tsx
@@ -1,7 +1,7 @@
-import React, { useMemo } from 'react'
-import { Form } from 'antd'
+import React, { useMemo, useState } from 'react'
+import { Form, Spin } from 'antd'
 import { FormProps } from 'antd/lib/form'
-import { IFormItemTopProps } from '../types'
+import { LifeCycleTypes, IFormItemTopProps, useFormEffects } from '../types'
 import { FormItemDeepProvider } from '../context'
 import { normalizeCol, isAntdV4 } from '../shared'
 import {
@@ -13,28 +13,36 @@ export const AntdSchemaFormAdaptor: React.FC<FormProps &
   IFormItemTopProps &
   PreviewTextConfigProps & { onSubmit: () => void }> = props => {
   const { inline, previewPlaceholder, onSubmit, onReset, ...rest } = props
+  const [loading, setLoading] = useState(false)
+  useFormEffects(($) => {
+    $(LifeCycleTypes.ON_FORM_CHANGE).subscribe(fs => {
+      setLoading(fs.loading)
+    })
+  })
   return (
     <FormItemDeepProvider {...props}>
       <PreviewText.ConfigProvider value={props}>
-        <Form
-          {...rest}
-          labelCol={normalizeCol(props.labelCol)}
-          wrapperCol={normalizeCol(props.wrapperCol)}
-          layout={inline ? 'inline' : props.layout}
-          onSubmit={onSubmit}
-          onReset={onReset}
-          component={useMemo(() => {
-            if (isAntdV4) {
-              return innerProps => {
-                return React.createElement('form', {
-                  ...innerProps,
-                  onSubmit,
-                  onReset
-                })
+        <Spin spinning={loading}>
+          <Form
+            {...rest}
+            labelCol={normalizeCol(props.labelCol)}
+            wrapperCol={normalizeCol(props.wrapperCol)}
+            layout={inline ? 'inline' : props.layout}
+            onSubmit={onSubmit}
+            onReset={onReset}
+            component={useMemo(() => {
+              if (isAntdV4) {
+                return innerProps => {
+                  return React.createElement('form', {
+                    ...innerProps,
+                    onSubmit,
+                    onReset
+                  })
+                }
               }
-            }
-          }, [])}
-        />
+            }, [])}
+          />
+        </Spin>
       </PreviewText.ConfigProvider>
     </FormItemDeepProvider>
   )
